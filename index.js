700b7488cfb883d9e282499dbca4646c7ae1d9eb
Luis Schekerka <luis@Luiss-MBP.fritz.box>
2020-01-13 15:58:25 +0100

    set up index.js file | added a dummy endpoint | added get for all users

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // implement your API here
+const express = require("express");
+const cors = require("cors");
+const { find, findById, add, remove, update } = require("./data/db");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/users", (req, res) => {
+  find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        success: false,
+        message: "The users information could not be retrieved."
+      });
+    });
+});
+
+
+
+app.get("/", (req, res) => {
+  res.json("this is the response");
+});
+
+app.listen(6000, () => {
+  console.log("listening on 6000");
+});
